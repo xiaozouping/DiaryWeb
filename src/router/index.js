b8e13d886eb562c875426ad69256e4678884a0c3
xiaozouping <1500609834@qq.com>
2020-04-07 00:28:57 +0800

    后台管理菜单初步

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Login from "../views/Login"
 import Index from "../views/Index"
 
@@ -8,11 +7,6 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/login',
       name: '登录页面',
